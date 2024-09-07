7641263bd3e54542be9814650f7a6e4c2bc67e64
halvornj <halvor@njastad.no>
2024-09-07 15:10:36 +0200

    added code to traverse multiple paginations of courses

--- scraperScripts/findCourseLinks.py
+++ scraperScripts/findCourseLinks.py
@@ -1,22 +1,32 @@
 from bs4 import BeautifulSoup
 import requests
 
-def scrapeAllUiOCourseLinks():
-#    URL = "https://www.uio.no/studier/emner/matnat/ifi/?filter.level=bachelor&filter.semester=h24&filter.teaching-language=norwegian"
-    URL = "https://www.uio.no/studier/emner/alle/?filter.level=bachelor&filter.semester=h24&filter.teaching-language=norwegian"
-    page = BeautifulSoup(requests.get(URL).text, "html.parser")
-    #finding all the href links on the main index page
+def getLinksForPage(page, links):
     courses = page.find_all('td',class_='vrtx-course-description-name')
-    links = []
     for link in courses:
         for a in link.find_all('a', href=True):
             links.append(a['href'])
-    print(f"{links=}")
+
+def scrapeAllUiOCourseLinks():
+    pageNo = 1
+    baseURL = "https://www.uio.no/studier/emner/alle/?filter.semester=h24&filter.teaching-language=norwegian"
+    links = []
+    page = BeautifulSoup(requests.get(baseURL).text, "html.parser")
+#finding all the href links on the main index page
+    nextButton = page.find_all('a',class_='vrtx-next')
+    while nextButton:
+        #print(f"{pageNo=}")
+        #print(f"{nextButton=}")
+        #print(f"--------------------------------------")
+        getLinksForPage(page, links)
+        pageNo = pageNo +1
+        page = BeautifulSoup(requests.get(baseURL+f"&page={pageNo}&u-page={pageNo}").text, "html.parser")
+        nextButton = page.find_all('a',class_='vrtx-next')
+    getLinksForPage(page, links)    
     print(f"{len(links)=}")
     return links
 
+
 def main():
     pass
-
-
-scrapeAllUiOCourseLinks()
+    #scrapeAllUiOCourseLinks()
